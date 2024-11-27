cfa4b8079c16ae7fe688e759099005cb4b95bcb8
Yunbo Lee <yblee85@gmail.com>
2024-11-27 01:17:42 -0500

    cleanup

--- src/linkedin/browser/index.js
+++ src/linkedin/browser/index.js
@@ -5,10 +5,10 @@ const { ConversationListener } = require('./conversation_listener.js');
  **/
 
 // search text "/voyager/api/voyagerMessagingGraphQL/graphql" in browser developer tool - network
-// example: https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerConversations.{RANDOM_STRING1}&variables=(mailboxUrn:{hostIdentityUrn})
+// example: https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerConversations.{RANDOM_STRING}&variables=(mailboxUrn:{hostIdentityUrn})
 
 // message history
-//https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerMessages.0c1bd47e37c67578e99250a711f0c18e&variables=(conversationUrn:urn:li:msg_conversation:(urn:li:fsd_profile:ACoAAAr2_UEBSOXtOcmGYsxIxANKTjZNr8dG4NE,2-NTc2OTRjNWQtZjRhNS00MzM2LWI2NzUtNzg2NzY1NWFjNjgxXzAxMA==))
+//https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerMessages.{RANDOM_STRING}&variables=(conversationUrn:urn:li:msg_conversation:({conversationEntityUrn}))
 const VoyagerMessengerConversationsQueryId = 'changeme-required';
 const VoyagerMessengerMessagesQueryId = 'changeme-optional';
 
